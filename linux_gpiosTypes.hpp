7457ea8c647852f11f252699a43ffeb97d707d24
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2018-05-08 13:31:20 -0300

    initial commit by rock-create-orogen

---
+++ linux_gpiosTypes.hpp
@@ -0,0 +1,21 @@
+#ifndef linux_gpios_TYPES_HPP
+#define linux_gpios_TYPES_HPP
+
+#include <vector>
+#include <base/Time.hpp>
+#include <raw_io/Digital.hpp>
+
+namespace linux_gpios {
+    struct Configuration
+    {
+        std::vector<int32_t> ids;
+    };
+    struct GPIOState
+    {
+        base::Time time;
+        std::vector<raw_io::Digital> state;
+    };
+}
+
+#endif
+
